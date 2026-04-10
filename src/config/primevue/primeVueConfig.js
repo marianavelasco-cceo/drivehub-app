730aeaa4cbe5c689331ca4ae17c1a16ec31511e4
Mariana Velasco <mariana.velasco@cceo.com.mx>
2026-04-09 19:54:13 -0600

    first commit :sparkles: initial project setup

---
+++ src/config/primevue/primeVueConfig.js
@@ -0,0 +1,7 @@
+import PrimeVuePt from "@/config/primevue/primeVuePt.js";
+import primeVueTheme from "./primeVueTheme";
+
+export default {
+    ...PrimeVuePt,
+    ...primeVueTheme
+}
---
+++ src/config/primevue/primeVueTheme.js
@@ -0,0 +1,54 @@
+import Aura from "@primeuix/themes/aura"
+import {definePreset} from "@primeuix/themes";
+
+export default {
+    theme: {
+        preset: definePreset(Aura, {
+            semantic: {
+                primary: {
+                    100: "var(--color-primary-100)",
+                    200: "var(--color-primary-200)",
+                    300: "var(--color-primary-300)",
+                    400: "var(--color-primary-400)",
+                    500: "var(--color-primary-500)",
+                    600: "var(--color-primary-600)",
+                    700: "var(--color-primary-700)",
+                    800: "var(--color-primary-800)",
+                    900: "var(--color-primary-900)",
+                },
+                secondary: {
+                    100: "var(--color-secondary-100)",
+                    200: "var(--color-secondary-200)",
+                    300: "var(--color-secondary-300)",
+                    400: "var(--color-secondary-400)",
+                    500: "var(--color-secondary-500)",
+                    600: "var(--color-secondary-600)",
+                    700: "var(--color-secondary-700)",
+                    800: "var(--color-secondary-800)",
+                    900: "var(--color-secondary-900)",
+                },
+                colorScheme: {
+                    light: {
+                        primary: {
+                            color: "var(--color-primary-500)",
+                            inverseColor: "#FFFFFF",
+                            hoverColor: "var(--color-primary-600)",
+                            activeColor: "var(--color-primary-700)",
+                        },
+                        secondary: {
+                            color: "var(--color-secondary-500)",
+                            inverseColor: "#FFFFFF",
+                            hoverColor: "var(--color-secondary-600)",
+                            activeColor: "var(--color-secondary-700)",
+                        },
+                    }
+                }
+            }
+        }),
+        options: {
+            prefix: 'p',
+            cssLayer: false,
+            darkModeSelector: false,
+        }
+    }
+}
